64bb4f8f62a9a03568cedc50974f18697db9c920
Scott <scottgerretsen@icloud.com>
2021-07-12 16:46:14 +0200

    removed redundant inline documentation

--- src/controllers/spreadsheetcontroller.js
+++ src/controllers/spreadsheetcontroller.js
@@ -1,15 +1,6 @@
 import { googleSheet } from '../models/spreadsheet';
 import { radbTemplateModel } from '../models/radbTemplateModel';
 
-/*
- * model notes:
- * 'columns[x].type' value is used to identify how to interpret the data in that column.
- *	e.g: when type == 'list' the data in that column should be interpreted as a comma-separated list.
- *		when type == 'options' the possible items should be limited to the values of another item 'options'.
- *		when type == 'number' the column should be interpreted as numbers.
- *		when type == 'undefined' the type 'string' is assumed.
-*/
-
 export const RadbSheet = undefined;
 
 export const getSpreadsheetRange = (req, res) => {
